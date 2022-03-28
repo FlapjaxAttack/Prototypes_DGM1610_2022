e37e2abb8acb426c86c653179280e60b2a36e5e1
Jack <flapjack.mail@gmail.com>
2022-03-27 22:55:48 -0600

    3rd Person Character Controller

---
+++ Prototypes/Assets/Scripts/PlayerControllerX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+    public float hInput;
+    public float fInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fInput = Input.GetAxis("Vertical");
+        hInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * fInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed * hInput);
+    }
+}
