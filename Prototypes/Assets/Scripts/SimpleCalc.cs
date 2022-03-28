e57ca8cc5caaaaa66351d4e56c71e4042766c6be
Jack <flapjack.mail@gmail.com>
2022-03-27 23:15:26 -0600

    3PCC Update

---
+++ Prototypes/Assets/Scripts/SimpleCalc.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCalc : MonoBehaviour
+{
+
+    public int numberA = 30;
+    public int numberB = 5;
+    public int numberC;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int total;
+
+        total = numberA + numberB;
+
+        Debug.Log(total);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
